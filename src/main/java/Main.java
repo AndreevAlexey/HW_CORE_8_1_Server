44355af1561d906e3a0511e2d72c455f30a4c6c3
Andreev Alexey <andreev-j@yandex.ru>
2021-09-24 21:28:58 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int port = 28989;
+        System.out.println("Serever started "+System.currentTimeMillis());
+        while(true) {
+
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("New connection port = "+clientSocket.getPort());
+
+            final String message = in.readLine();
+            System.out.println(message);
+            out.println(String.format("Hi %s, your port is %d", message, clientSocket.getPort()));
+
+            serverSocket.close();
+        }
+    }
+}
