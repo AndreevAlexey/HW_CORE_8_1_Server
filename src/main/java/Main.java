fb1620a71ab953e6b6214c6acb8357ae2025cacc
Andreev Alexey <andreev-j@yandex.ru>
2021-09-26 20:11:34 +0700

    Update: try with resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,22 @@ import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int port = 28989;
         System.out.println("Serever started "+System.currentTimeMillis());
         while(true) {
-
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            System.out.println("New connection port = "+clientSocket.getPort());
-
-            final String message = in.readLine();
-            System.out.println(message);
-            out.println(String.format("Hi %s, your port is %d", message, clientSocket.getPort()));
-
-            serverSocket.close();
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            ) {
+                System.out.println("New connection port = " + clientSocket.getPort());
+                final String message = in.readLine();
+                System.out.println(message);
+                out.println(String.format("Hi %s, your port is %d", message, clientSocket.getPort()));
+            } catch (IOException exp) {
+                exp.printStackTrace();
+            }
         }
     }
 }
